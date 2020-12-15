d7efc9af60e674d5fc8e75a164646b7025526324
Tania Santos <Tania.Guerreiro.Santos@formandos.cinel.pt>
2020-12-15 12:30:12 +0000

    Inicialização do projecto

---
+++ LojaCET49/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaCET49.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
